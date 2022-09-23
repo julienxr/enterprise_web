2965ac601448ff38b11b5057b67427e1b795c436
Xavier Julien <julienxr@gmail.com>
2022-09-23 13:33:05 -0500

    feat: finalizing version one of layout scheme

--- src/components/layouts/navigation/main/Main.mocks.ts
+++ src/components/layouts/navigation/main/Main.mocks.ts
@@ -1,7 +1,9 @@
 import { IMain } from './Main';
 // Mock props that matches the shape of the Base Interface so to quickly test a version of the component either in app or storybook
 
-const base: IMain = {};
+const base: IMain = {
+  children: '<h1>HI</h1>',
+};
 
 export const mockMainProps = {
   base,
--- src/components/layouts/navigation/navbar/Navbar.mocks.ts
+++ src/components/layouts/navigation/navbar/Navbar.mocks.ts
@@ -1,9 +1,7 @@
 import { INavbar } from './Navbar';
 // Mock props that matches the shape of the Base Interface so to quickly test a version of the component either in app or storybook
 
-const base: INavbar = {
-  sampleTextProp: 'Greetings Earthlings',
-};
+const base: INavbar = {};
 
 export const mockNavbarProps = {
   base,
